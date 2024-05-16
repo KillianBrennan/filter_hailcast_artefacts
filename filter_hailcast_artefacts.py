ad3aa76aeb5dc073ab7edb632a52d8c8d1758ffa
Killian Brennan <kbrennan@env.ethz.ch>
2024-05-16 17:31:29 +0200

    working implementation,  loads daily files from yesterday, today & tomorrow, does not work for last day in simulation.

--- filter_hailcast_artefacts.py
+++ filter_hailcast_artefacts.py
@@ -24,6 +24,10 @@ python /home/kbrennan/filter_hailcast_artefacts/filter_hailcast_artefacts.py /ne
 Killian P. Brennan
 14.05.2024
 ---------------------------------------------------------
+todos:
+change data source to the 5min data
+add more temporal ambiguity (+/- 4 time steps)
+add threshold on slope angle (inflated)
 
 """
 
@@ -38,33 +42,132 @@ import scipy.ndimage as ndimage
 from skimage.morphology import disk
 
 
-def filter_hailcast_artefacts(inpath, outpath, start_day, end_day):
+def filter_hailcast_artefacts(
+    inpath,
+    outpath,
+    start_day,
+    end_day,
+    temporal_ambiguity=4,  # time steps
+    threshold=2,  # mm/h
+    kernel_size_km=17,  # km
+):
     start_day = pd.to_datetime(start_day, format="%Y%m%d")
     end_day = pd.to_datetime(end_day, format="%Y%m%d")
 
     daylist = pd.date_range(start_day, end_day)
+    day_m_1 = start_day - pd.DateOffset(days=1)
 
     # make output directory
     os.makedirs(outpath, exist_ok=True)
 
-    for day in daylist:
-        day_str = day.strftime("%Y%m%d")
-        print(day_str)
-        original = xr.open_dataset(os.path.join(inpath,'lffd'+day_str+'_0606.nz'))
-        filtered = apply_filter(original)
+    for i, day in enumerate(daylist):
+        today_str = day.strftime("%Y%m%d")
+        print(today_str)
+        if i == 0:
+            yesterday = xr.open_dataset(
+                os.path.join(inpath, "lffd" + day_m_1.strftime("%Y%m%d") + "_0606.nz")
+            )
+        else:
+            yesterday = today
+        if i == 0:
+            today = xr.open_dataset(
+                os.path.join(inpath, "lffd" + today_str + "_0606.nz")
+            )
+        else:
+            today = tomorrow
 
-        filtered.DHAIL_MX_filtered.to_netcdf(os.path.join(outpath, 'lffd'+day_str+'_0606.nc'))
+        tomorrow_path = os.path.join(
+            inpath, "lffd" + daylist[i + 1].strftime("%Y%m%d") + "_0606.nz"
+        )
+        if os.path.exists(tomorrow_path):
+            tomorrow = xr.open_dataset(tomorrow_path)
+        else:
+            print('warning: last day in time range, filling with high precip value - no hail will be filtered for the last n=spacial_ambiguity time steps')
+            # for the last day, the next day does not exist, fill with high precip value so no hail is filtered
+            tomorrow = xr.full_like(today, 100) 
 
+        combined = xr.concat(
+            [
+                yesterday.isel(time=slice(-(temporal_ambiguity + 1), -1)),
+                today,
+                tomorrow.isel(time=slice(0, temporal_ambiguity + 1)),
+            ],
+            dim="time",
+        )
+        TOT_PREC = combined.TOT_PREC
+        DHAIL_MX = today.DHAIL_MX
+        filtered = filter_daily_field(
+            DHAIL_MX,
+            TOT_PREC,
+            threshold=threshold,
+            kernel_size_km=kernel_size_km,
+            temporal_ambiguity=temporal_ambiguity,
+        )
+
+        # write compressed netcdf
+        filtered.to_netcdf(
+            os.path.join(outpath, "lffd" + today_str + "_0606.nc"),
+            format="NETCDF4",
+            engine="netcdf4",
+            encoding={"DHAIL_MX": {"zlib": True, "complevel": 9}},
+        )
     return
 
 
-def apply_filter(
-    ds,
-    threshold=5,  # mm/h
-    kernel_size=15,  # km
-    temporal_ambiguity=2,  # time steps
+# def filter_5min_climate(inpath, outpath, start_day, end_day):
+#     temporal_ambiguity = 4  # time steps
+#     threshold = 2  # mm/h
+#     kernel_size = 8  # grid points
+
+#     # list all files in the input directory
+#     files = os.listdir(inpath)
+#     files = [f for f in files if f.endswith(".nc")]
+#     files = [f for f in files if f.startswith("lffd")]
+#     files = sorted(files)
+#     # format is lffdYYYYMMDDHHmmss.nc
+
+#     for i, f in enumerate(files):
+#         if int(f[4:12]) >= (int(end_day)):
+#             print("reached end of specified time range")
+#             break
+#         print(f)
+#         if int(f[4:12]) < int(start_day):
+#             print("skipping, not in specified time range")
+#             continue
+#         # print("processing")
+#         ds = xr.open_mfdataset(
+#             [
+#                 os.path.join(inpath, f)
+#                 for f in files[i - temporal_ambiguity : i + temporal_ambiguity + 1]
+#             ]
+#         )
+#         rr = ds.TOT_PREC.max(dim="time") * 12
+#         DHAIL_MX = ds.DHAIL_MX.isel(time=temporal_ambiguity)
+#         m = build_mask(rr, threshold=threshold, kernel_size=kernel_size)
+#         DHAIL_MX_filtered = DHAIL_MX.where(m, 0)
+#         # add back the time dimension
+#         # DHAIL_MX_filtered = DHAIL_MX_filtered.expand_dims(dim={"time": ds.DHAIL_MX.isel(time=temporal_ambiguity).time})
+#         DHAIL_MX_filtered = DHAIL_MX_filtered.expand_dims("time")
+#         DHAIL_MX_filtered.to_netcdf(os.path.join(outpath, f))
+
+#     return
+
+
+def build_mask(rr, threshold=5, kernel_size=8):
+    kernel = disk(kernel_size)
+    m = rr > threshold
+    m = ndimage.binary_dilation(m, structure=kernel)
+    return m
+
+
+def filter_daily_field(
+    DHAIL_MX,
+    TOT_PREC,
+    threshold=2,  # mm/h
+    kernel_size_km=17,  # km
+    temporal_ambiguity=4,  # time steps
 ):
-    '''
+    """
     Filter out hailcast artefacts using a threshold and a binary dilatation
     Parameters
     ----------
@@ -72,7 +175,7 @@ def apply_filter(
     hailcast data
     threshold : float
     threshold for hailcast data
-    kernel_size : int
+    kernel_size_km : int
     kernel size for binary dilatation
     temporal_ambiguity : int
     number of time steps to consider before and after current time step
@@ -81,71 +184,80 @@ def apply_filter(
     -------
     ds : xarray dataset
     hailcast data with artefacts removed
-        
-        '''
 
-    ds["DHAIL_MX_filtered"] = ds["DHAIL_MX"]
+    """
+    DHAIL_MX_filtered = xr.full_like(DHAIL_MX, 0)
 
     # get dt from df.time
-    # dt = pd.to_timedelta(ds.time.diff(dim="time").mean().values).total_seconds() / 3600
-    dt = 1 / 12  # 5 min
+    dt = (
+        pd.to_timedelta(DHAIL_MX.time.diff(dim="time").mean().values).total_seconds()
+        / 3600
+    )
+    # dt = 1 / 12  # 5 min
 
     # get grid spacing from df.lat
     r_earth = 6371  # km
-    lat = ds.lat.values
+    lat = DHAIL_MX.lat.values
     dlat = np.abs(lat[1] - lat[0])
     dx = r_earth * np.radians(dlat)
     # get grid spacing in km
     dx = dx.mean()
-    kernel_size = int(np.round(kernel_size / dx))
-    kernel = disk(kernel_size)
+    kernel_size_gp = int(np.round(kernel_size_km / dx))
 
-    mask = xr.full_like(ds.TOT_PREC, False)
+    mask = xr.full_like(DHAIL_MX, False)
 
-    for i in range(len(ds.time)):
-        start = i - temporal_ambiguity
-        end = i + temporal_ambiguity + 1
-        # make sure the start and end indices are within the time dimension
-        if start < 0:
-            start = 0
-        if end > len(ds.time):
-            end = len(ds.time)
-        rr = ds.TOT_PREC.isel(time=slice(start, end)).max(dim="time")
-        rr = rr / dt
-        m = rr > threshold
-        m = ndimage.binary_dilation(m, structure=kernel)
+    for i in range(len(DHAIL_MX.time)):
+        # tot_prec start index is offset from dhail start index by temporal ambiguity
+        rr = (
+            TOT_PREC.isel(time=slice(i, i + 2 * temporal_ambiguity + 1)).max(dim="time")
+            / dt
+        )
+        m = build_mask(rr, threshold=threshold, kernel_size=kernel_size_gp)
         mask[i] = m.copy()
 
-    ds["DHAIL_MX_filtered"] = ds["DHAIL_MX"].where(mask, 0)
+    DHAIL_MX_filtered = DHAIL_MX.where(mask, 0)
 
-    return ds
+    # add filtering attributes
+    DHAIL_MX_filtered.attrs["filter_threshold_mm_per_h"] = threshold
+    DHAIL_MX_filtered.attrs["filter_kernel_size_gp"] = kernel_size_gp
+    DHAIL_MX_filtered.attrs["filter_kernel_size_km"] = kernel_size_km
+    DHAIL_MX_filtered.attrs["filter_temporal_ambiguity_time_steps"] = temporal_ambiguity
+
+    return DHAIL_MX_filtered
 
 
 if __name__ == "__main__":
 
     parser = ArgumentParser(description="filter orographic hailcast artefacts")
 
+    parser.add_argument("inpath", type=str, help="path to the input directory")
+    parser.add_argument("outpath", type=str, help="path to the output directory")
+    parser.add_argument("start_day", type=str, help="start day in format YYYYMMDD")
+    parser.add_argument("end_day", type=str, help="end day in format YYYYMMDD")
     parser.add_argument(
-        'inpath',
-        type=str,
-        help='path to the input directory'
+        "--threshold", type=float, default=2, help="threshold for hailcast data"
     )
     parser.add_argument(
-        'outpath',
-        type=str,
-        help='path to the output directory'
+        "--kernel_size_km",
+        type=int,
+        default=17,
+        help="kernel size for binary dilatation",
     )
     parser.add_argument(
-        'start_day',
-        type=str,
-        help='start day in format YYYYMMDD'
-    )
-    parser.add_argument(
-        'end_day',
-        type=str,
-        help='end day in format YYYYMMDD'
+        "--temporal_ambiguity",
+        type=int,
+        default=4,
+        help="number of time steps to consider before and after current time step",
     )
 
     args = parser.parse_args()
 
-    filter_hailcast_artefacts(args.inpath, args.outpath, args.start_day, args.end_day)
+    filter_hailcast_artefacts(
+        args.inpath,
+        args.outpath,
+        args.start_day,
+        args.end_day,
+        threshold=args.threshold,
+        kernel_size_km=args.kernel_size_km,
+        temporal_ambiguity=args.temporal_ambiguity,
+    )
--- plot_climate_footprints.py
+++ plot_climate_footprints.py
@@ -1,53 +1,53 @@
+'''
+---------------------------------------------------------
+EXAMPLE CALL
+python /home/kbrennan/filter_hailcast_artefacts/plot_climate_footprints.py /net/litho/atmosdyn2/kbrennan/data/climate/present/ /net/litho/atmosdyn2/kbrennan/data/climate/present/plots/potential_artefacts/ 20210102 20221231
+---------------------------------------------------------
+'''
+
+
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+import pandas as pd
 
+from argparse import ArgumentParser
 
-def main():
-    root_path = "/home/kbrennan/phd/data/climate/present"
-    footprint_path = os.path.join(root_path, "hail_footprints")
-    horizontal_path = os.path.join(root_path, "5min_2D")
-    filtered_path = os.path.join(root_path, "filtered_hailcast")
-    const_path = os.path.join(root_path, "lffd20101019000000c.nc")
+def main(inpath, outpath, start_day, end_day):
+
+    # make sure the output directory exists
+    os.makedirs(outpath, exist_ok=True)
+
+    const_path = os.path.join(inpath, "lffd20101019000000c.nc")
     const = xr.open_dataset(const_path)
 
-    footprint_files = os.listdir(footprint_path)
-    footprint_files.sort()
-    # only nc files
-    footprint_files = [f for f in footprint_files if f.endswith(".nc")]
-    # only the ones that start with ffd2021
-    footprint_files = [f for f in footprint_files if f.startswith("lffd2021")]
-    footprint_files = footprint_files[11:]
-    plotdir = os.path.join(root_path, "plots", "potential_artefacts")
-    if not os.path.exists(plotdir):
-        os.makedirs(plotdir)
+    origpath = os.path.join(inpath, "5min_2D")
+    filteredpath = os.path.join(inpath, "filtered_hailcast")
 
-    for f in footprint_files:
-        day = f.split("_")[0][4:]
-        print(day)
-        footprint = xr.open_dataset(os.path.join(footprint_path, f))
-        filtered = xr.open_dataset(
-            os.path.join(filtered_path, "lffd" + day + "_0606.nc")
-        )
+    start_day = pd.to_datetime(start_day, format="%Y%m%d")
+    end_day = pd.to_datetime(end_day, format="%Y%m%d")
+    days = pd.date_range(start_day, end_day, freq="D")
+    for day in days:
+        day_str = day.strftime("%Y%m%d")
+        print(day_str)
 
-        try:
-            horizontal = xr.open_mfdataset(
-                os.path.join(horizontal_path, "*" + day + "*.nz")
-            )
-        except:
-            print("No file found for " + day)
-            continue
+        filtered = xr.open_dataset(os.path.join(filteredpath, 'lffd' + day_str + "_0606.nc"))
+        orig = xr.open_dataset(os.path.join(origpath, 'lffd' + day_str + "_0606.nz"))
 
-        diff = footprint.DHAIL_MX.squeeze() - filtered.DHAIL_MX_filtered.max(dim="time")
-        n_artefacts = np.sum(diff.values > 5)
+        orig_footprint = orig.max(dim="time")
+        filtered_footprint = filtered.max(dim="time")
+
+        diff = orig_footprint.DHAIL_MX - filtered_footprint.DHAIL_MX
+
+        # n_artefacts = np.sum(diff.values > 5)
         # plot
         fig, ax = plt.subplots(1, 1, figsize=(20, 16))
         pmesh = ax.pcolormesh(
-            horizontal.rlon,
-            horizontal.rlat,
-            horizontal.TOT_PREC.max(dim="time") * 60 / 5,
+            orig_footprint.rlon,
+            orig_footprint.rlat,
+            orig_footprint.TOT_PREC *12,
             vmax=20,
             vmin=0,
             cmap="Blues",
@@ -56,36 +56,36 @@ def main():
             const.rlon,
             const.rlat,
             const.HSURF.squeeze(),
-            levels=[2000],
-            colors="black",
+            levels=[1000,2000],
+            colors=[(0, 0, 0, 0.5), (0, 0, 0, 1)],
             linewidths=0.5,
         )
         ax.contour(
-            footprint.rlon,
-            footprint.rlat,
-            footprint.DHAIL_MX.squeeze(),
+            orig_footprint.rlon,
+            orig_footprint.rlat,
+            orig_footprint.DHAIL_MX,
             levels=[5],
             colors="red",
             linewidths=0.5,
         )
 
         # print number of pixels with artefacts
-        if n_artefacts > 0:
-            print("n_artefacts: " + str(n_artefacts))
-        # add text to plot
-        ax.text(
-            0.01,
-            0.99,
-            "n_artefacts: " + str(n_artefacts),
-            transform=ax.transAxes,
-            fontsize=10,
-            verticalalignment="top",
-            horizontalalignment="left",
-        )
+        # if n_artefacts > 0:
+        #     print("n_artefacts: " + str(n_artefacts))
+        # # add text to plot
+        # ax.text(
+        #     0.01,
+        #     0.99,
+        #     "n_artefacts: " + str(n_artefacts),
+        #     transform=ax.transAxes,
+        #     fontsize=10,
+        #     verticalalignment="top",
+        #     horizontalalignment="left",
+        # )
 
         ax.contourf(
-            filtered.rlon,
-            filtered.rlat,
+            diff.rlon,
+            diff.rlat,
             diff,
             levels=[5, 50],
             colors="tab:orange",
@@ -101,8 +101,129 @@ def main():
         # ax.set_ylim(45, 48)
 
         ax.set_title(day)
-        plt.savefig(os.path.join(plotdir, day + ".png"), dpi=600)
+        plt.savefig(os.path.join(outpath, day_str + ".png"), dpi=600)
 
 
 if __name__ == "__main__":
-    main()
+
+    parser = ArgumentParser(description="filter orographic hailcast artefacts")
+
+    parser.add_argument("inpath", type=str, help="path to the input directory")
+    parser.add_argument("outpath", type=str, help="path to the output directory")
+    parser.add_argument("start_day", type=str, help="start day in format YYYYMMDD")
+    parser.add_argument("end_day", type=str, help="end day in format YYYYMMDD")
+
+    args = parser.parse_args()
+
+    main(args.inpath, args.outpath, args.start_day, args.end_day)
+
+
+
+# import os
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import xarray as xr
+
+
+# def main():
+#     root_path = "/home/kbrennan/phd/data/climate/present"
+#     footprint_path = os.path.join(root_path, "hail_footprints")
+#     horizontal_path = os.path.join(root_path, "5min_2D")
+#     filtered_path = os.path.join(root_path, "filtered_hailcast")
+#     const_path = os.path.join(root_path, "lffd20101019000000c.nc")
+#     const = xr.open_dataset(const_path)
+
+#     footprint_files = os.listdir(footprint_path)
+#     footprint_files.sort()
+#     # only nc files
+#     footprint_files = [f for f in footprint_files if f.endswith(".nc")]
+#     # only the ones that start with ffd2021
+#     footprint_files = [f for f in footprint_files if f.startswith("lffd2021")]
+#     footprint_files = footprint_files[11:]
+#     plotdir = os.path.join(root_path, "plots", "potential_artefacts")
+#     if not os.path.exists(plotdir):
+#         os.makedirs(plotdir)
+
+#     for f in footprint_files:
+#         day = f.split("_")[0][4:]
+#         print(day)
+#         footprint = xr.open_dataset(os.path.join(footprint_path, f))
+#         filtered = xr.open_dataset(
+#             os.path.join(filtered_path, "lffd" + day + "_0606.nc")
+#         )
+
+#         try:
+#             horizontal = xr.open_mfdataset(
+#                 os.path.join(horizontal_path, "*" + day + "*.nz")
+#             )
+#         except:
+#             print("No file found for " + day)
+#             continue
+
+#         diff = footprint.DHAIL_MX.squeeze() - filtered.DHAIL_MX_filtered.max(dim="time")
+#         n_artefacts = np.sum(diff.values > 5)
+#         # plot
+#         fig, ax = plt.subplots(1, 1, figsize=(20, 16))
+#         pmesh = ax.pcolormesh(
+#             horizontal.rlon,
+#             horizontal.rlat,
+#             horizontal.TOT_PREC.max(dim="time") * 60 / 5,
+#             vmax=20,
+#             vmin=0,
+#             cmap="Blues",
+#         )
+#         ax.contour(
+#             const.rlon,
+#             const.rlat,
+#             const.HSURF.squeeze(),
+#             levels=[2000],
+#             colors="black",
+#             linewidths=0.5,
+#         )
+#         ax.contour(
+#             footprint.rlon,
+#             footprint.rlat,
+#             footprint.DHAIL_MX.squeeze(),
+#             levels=[5],
+#             colors="red",
+#             linewidths=0.5,
+#         )
+
+#         # print number of pixels with artefacts
+#         if n_artefacts > 0:
+#             print("n_artefacts: " + str(n_artefacts))
+#         # add text to plot
+#         ax.text(
+#             0.01,
+#             0.99,
+#             "n_artefacts: " + str(n_artefacts),
+#             transform=ax.transAxes,
+#             fontsize=10,
+#             verticalalignment="top",
+#             horizontalalignment="left",
+#         )
+
+#         ax.contourf(
+#             filtered.rlon,
+#             filtered.rlat,
+#             diff,
+#             levels=[5, 50],
+#             colors="tab:orange",
+#             alpha=0.5,
+#         )
+
+#         # add colorbar
+#         cbar = fig.colorbar(pmesh, ax=ax)
+#         cbar.set_label("max precipitation rate (mm/h)")
+
+#         # limit to swiss lat /lon
+#         # ax.set_xlim(5, 11)
+#         # ax.set_ylim(45, 48)
+
+#         ax.set_title(day)
+#         plt.savefig(os.path.join(plotdir, day + ".png"), dpi=600)
+
+
+# if __name__ == "__main__":
+#     main()
